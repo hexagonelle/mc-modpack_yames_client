27c5866f061dbc3ea28803a914bed6519d40d9ad
Elle <115524533+hexagonelle@users.noreply.github.com>
2025-10-30 11:11:17 -0700

    Initial commit

---
+++ figura/avatars/hornyHex/script.lua
@@ -0,0 +1,6 @@
+-- Auto generated script file --
+
+--hide vanilla model
+vanilla_model.PLAYER:setVisible(true)
+vanilla_model.LEFT_SLEEVE:setVisible(false)
+vanilla_model.RIGHT_SLEEVE:setVisible(false)
