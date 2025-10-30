c5b2a051d158e1851ca082e721a12f0493744666
Elle <115524533+hexagonelle@users.noreply.github.com>
2025-10-30 11:20:00 -0700

    Removed figura avatar files (but not the mod)

--- figura/avatars/hornyHex/script.lua
+++
@@ -1,6 +0,0 @@
--- Auto generated script file --
-
---hide vanilla model
-vanilla_model.PLAYER:setVisible(true)
-vanilla_model.LEFT_SLEEVE:setVisible(false)
-vanilla_model.RIGHT_SLEEVE:setVisible(false)
